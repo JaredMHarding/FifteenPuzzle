9ce204b1d5752ea4ae657d51d359c2ffef26b2d7
Jared Micheal Harding <jharding@ecs323-6.labs.encs>
2018-04-11 13:57:37 -0700

    Completed most of the FifteenPuzzle.swift functions and filled in functions in BoardView.swift and ViewController.swift

--- FifteenPuzzle/FifteenBoard.swift
+++ FifteenPuzzle/FifteenBoard.swift
@@ -21,37 +21,64 @@ class FifteenBoard {
         
     }
     
-    func getTile() -> Int {
-        
+    func getTile(atRow r: Int, atColumn c: Int) -> Int {
+        return state[r][c]
     }
     
-    func getRowAndColumn(forTile tile: Int) -> (row: Int, column: Int) {
-        
+    func getRowAndColumn(forTile tile: Int) -> (row: Int, column: Int)? {
+        if ((tile < 0) || (tile > 15)) {
+            return nil
+        }
+        for r in 0 ..< 4 {
+            for c in 0 ..< 4 {
+                if (state[r][c] == tile) {
+                    return (r,c)
+                }
+            }
+        }
+        // to make the compiler happy
+        return nil
     }
     
     func isSolved() -> Bool {
-        
+        var tile = 1
+        for r in 0 ..< 4 {
+            for c in 0 ..< 4 {
+                if (state[r][c] != tile) {
+                    return false
+                }
+                tile = (tile + 1) % 16
+            }
+        }
+        return true
     }
     
-    func canSlideTileUp(atRow r : Int, Column c : Int) -> Bool {
-        
+    func canSlideTileUp(atRow r : Int, atColumn c : Int) -> Bool {
+        return (r >= 1) && (r <= 3) && (state[r-1][c] == 0)
     }
     
-    func canSlideTileDown(atRow r : Int, Column c : Int) -> Bool {
-        
+    func canSlideTileDown(atRow r : Int, atColumn c : Int) -> Bool {
+        return (r >= 0) && (r <= 2) && (state[r+1][c] == 0)
     }
     
-    func canSlideTileLeft(atRow r : Int, Column c : Int) -> Bool {
-        
+    func canSlideTileLeft(atRow r : Int, atColumn c : Int) -> Bool {
+        return (c >= 1) && (c <= 3) && (state[r][c-1] == 0)
     }
     
-    func canSlideTileRight(atRow r : Int, Column c : Int) -> Bool {
-        
+    func canSlideTileRight(atRow r : Int, atColumn c : Int) -> Bool {
+        return (c >= 0) && (c <= 2) && (state[r][c+1] == 0)
     }
-    func canSlideTile(atRow r : Int, Column c : Int) -> Bool {
-        
+    /*
+    func canSlideTile(atRow r : Int, atColumn c : Int) -> Bool {
+        return (
+            canSlideTileUp(atRow:r,atColumn:c) ||
+            canSlideTileDown(atRow:r,atColumn:c) ||
+            canSlideTileLeft(atRow:r,atColumn:c) ||
+            canSlideTileRight(atRow:r,atColumn:c)
+        )
     }
-    func slideTile(atRow r : Int, Column c: Int) {
+    */
+    func slideTile(atRow r : Int, atColumn c: Int) {
         
     }
 }
